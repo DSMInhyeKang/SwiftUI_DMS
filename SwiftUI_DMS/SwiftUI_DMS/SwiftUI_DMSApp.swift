3d138d6c7ba054fa70747a3b78f05cdb1ad4c4e7
InhyeKang <kanginhae0826@gmail.com>
2022-08-24 19:47:29 +0900

    🌟 :: 프로젝트 생성

---
+++ SwiftUI_DMS/SwiftUI_DMS/SwiftUI_DMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DMSApp.swift
+//  SwiftUI_DMS
+//
+//  Created by 강인혜 on 2022/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
